075eadb61ab227159dbd563207c72d79534d307a
Matthew R. Wilson <mwilson@mattwilson.org>
2011-05-26 18:18:17 -0700

    Pass jslint

--- flashcards.js
+++ flashcards.js
@@ -1,3 +1,7 @@
+/*jslint white: true, onevar: true, browser: true, undef: true, nomen: true,
+	regexp: true, plusplus: false, bitwise: true, newcap: true, maxerr: 50,
+	maxlen: 80, indent: 4 */
+
 var cards = [
 	["1st Singular", [
 		["porta, -ae (f) -> Sg. Nom.", "porta"],
@@ -13,40 +17,38 @@ var cards = [
 		["porta, -ae (f) -> Pl. Abl.", "portīs"] ] ]
 ];
 
-var activeCards = new Array();
+var activeCards = [];
 
 // For now, we will just make all cards active. In the future,
 // we will allow the user to choose the sets of cards they want
 // to be quizzed on.
 activeCards = activeCards.concat(cards[0][1], cards[1][1]);
 
-var sequence = new Array();
+var sequence = [];
 
 var currentCard = 0;
 var pos = 0;
 
+function isValueInSequence(theory, sequence) {
+	var i;
+	for (i = 0; i < sequence.length; i++) {
+		if (sequence[i] === theory) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function generateRandomSequence(size) {
-	var sequence = new Array();
-	var i = size;
-	var j;
-	while(i>0) {
-		j = Math.floor(Math.random()*size);
-		if(!isValueInSequence(j, sequence)) {
+	var sequence = [], i = size, j;
+	while (i > 0) {
+		j = Math.floor(Math.random() * size);
+		if (!isValueInSequence(j, sequence)) {
 			sequence.push(j);
 			i--;
 		}
 	}
 	return sequence;
-}
-
-function isValueInSequence(theory, sequence) {
-	var i;
-	for(i=0; i<sequence.length; i++) {
-		if(sequence[i]==theory) {
-			return true;
-		}
-	}
-	return false;
 }
 
 function init() {
@@ -58,7 +60,7 @@ function init() {
 function go() {
 	pos++;
 	
-	if(pos>=sequence.length) {
+	if (pos >= sequence.length) {
 		init();
 	}
 	
@@ -66,28 +68,29 @@ function go() {
 	document.getElementById('question').innerHTML = activeCards[currentCard][0];
 	document.getElementById('feedback').innerHTML = "";
 	document.getElementById('answer').value = "";
-	document.getElementById('status').innerHTML = (pos+1) + " of " + sequence.length;
+	document.getElementById('status').innerHTML = (pos + 1) + " of " +
+		sequence.length;
 	document.getElementById('answer').focus();
-}
-
-function handleKeyPress(e) {
-	if(e.keyCode == 13) {
-		checkAnswer();
-		return false;
-	} else {
-		return true;
-	}
 }
 
 function checkAnswer() {
 	var userAnswer = document.getElementById('answer').value;
-	if(userAnswer == activeCards[currentCard][1]) {
+	if (userAnswer === activeCards[currentCard][1]) {
 		document.getElementById('feedback').innerHTML = "Correct!";
 		document.getElementById('next').focus();
 	} else {
 		document.getElementById('feedback').innerHTML = "Incorrect.";
 		document.getElementById('answer').focus();
 		document.getElementById('answer').select();
+	}
+}
+
+function handleKeyPress(e) {
+	if (e.keyCode === 13) {
+		checkAnswer();
+		return false;
+	} else {
+		return true;
 	}
 }
 
@@ -98,32 +101,30 @@ function insertChar(character) {
 
 function doSetup() {
 	var i;
-	activeCards = new Array();
-	for(i=0; i<document.checkboxes.cardset.length; i++) {
-		if(document.checkboxes.cardset[i].checked) {
+	activeCards = [];
+	for (i = 0; i < document.checkboxes.cardset.length; i++) {
+		if (document.checkboxes.cardset[i].checked) {
 			activeCards = activeCards.concat(cards[i][1]);
 		}
 	}
 
 	document.getElementById('setup').style.display = 'none';
 	document.getElementById('content').style.display = 'block';
-	sequence = new Array();
+	sequence = [];
 	go();
 }
 
 function startSetup() {
-	var br;
-	var checkbox;
-	
-	var i;
-	for(i=0; i<cards.length; i++) {
+	var br, checkbox, i;
+	for (i = 0; i < cards.length; i++) {
 		br = document.createElement('br');
 		checkbox = document.createElement('input');
 		checkbox.type = "checkbox";
 		checkbox.name = "cardset";
 		document.getElementById('checkboxes').appendChild(checkbox);
 		document.getElementById('checkboxes').appendChild(
-			document.createTextNode(cards[i][0]));
+			document.createTextNode(cards[i][0])
+		);
 		document.getElementById('checkboxes').appendChild(br);
 	}
 }
@@ -132,4 +133,6 @@ function resumeSetup() {
 	document.getElementById('setup').style.display = 'block';
 	document.getElementById('content').style.display = 'none';
 }
+
+
 
